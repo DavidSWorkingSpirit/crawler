ad57670210f5654becb7be4ab2e2c08f6629c0fc
DavidSWorkingSpirit <49674149+DavidSWorkingSpirit@users.noreply.github.com>
2019-11-25 15:14:51 +0100

    Update HtmlCrawler.java

--- src/main/java/com/example/crawlertest/domein/HtmlCrawler.java
+++ src/main/java/com/example/crawlertest/domein/HtmlCrawler.java
@@ -53,7 +53,7 @@ public class HtmlCrawler extends WebCrawler {
             Set<WebURL> links = htmlParseData.getOutgoingUrls();
 
             if (content.toLowerCase().contains(zoekopdracht.getZoekterm().toLowerCase()) &&
-               Pattern.compile("vacature/\\d{7}").matcher(url).find()) {
+                Pattern.compile("vacature/\\d{7}").matcher(url).find()) {
 
                 resultaat.setTitel(titel);
                 resultaat.setTekst(content);
