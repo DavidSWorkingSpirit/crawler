bff4b1fd0420533bbb53723f39c1dca35efeeb15
DavidSWorkingSpirit <d.smit@workingspirit.nl>
2019-11-27 13:38:01 +0100

    Admin kan websites wijzigien

--- src/main/java/com/example/crawlertest/controller/WebsiteController.java
+++ src/main/java/com/example/crawlertest/controller/WebsiteController.java
@@ -34,4 +34,21 @@ public class WebsiteController {
 
         return websiteService.geefAlleWebsites();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Website> geefWebsite(@PathVariable Long id) {
+
+        return websiteService.geefWebsiteOpId(id).map(website -> ResponseEntity.ok(website))
+                                                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity wijzigWebsite(@PathVariable Long id, @RequestBody Website website) {
+
+        if (websiteService.wijzigWebsite(id, website)) {
+            return ResponseEntity.ok().build();
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
 }
--- src/main/java/com/example/crawlertest/services/WebsiteService.java
+++ src/main/java/com/example/crawlertest/services/WebsiteService.java
@@ -5,7 +5,10 @@ import com.example.crawlertest.repositories.WebsiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class WebsiteService {
@@ -30,5 +33,33 @@ public class WebsiteService {
     public List<Website> geefAlleWebsites() {
 
         return websiteRepository.findAll();
+    }
+
+    public Optional<Website> geefWebsiteOpId(Long id) {
+
+        return websiteRepository.findById(id);
+    }
+
+    public boolean wijzigWebsite(Long id, Website website) {
+        Optional<Website> teWijzigenWebsite = websiteRepository.findById(id);
+
+        if (teWijzigenWebsite.isPresent() && !isGelijk(teWijzigenWebsite.get(), website)) {
+            websiteRepository.save(website);
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isGelijk(Website bestaand, Website gewijzigd) {
+        boolean gelijkeNaam = bestaand.getNaam().equals(gewijzigd.getNaam());
+        boolean gelijkeUrl = bestaand.getUrl().equals(gewijzigd.getUrl());
+        boolean gelijkFilter = bestaand.getFilter().equals(gewijzigd.getFilter());
+
+        if (gelijkeNaam && gelijkeUrl && gelijkFilter) {
+            return true;
+        }
+
+        return false;
     }
 }
