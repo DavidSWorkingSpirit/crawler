1ac075327a9d06e329f105f2340e5f9fa83fe5ea
KikkertJ <j.kikkert@workingspirit.nl>
2019-11-28 14:07:46 +0100

    omzetten vacature naar vacatureDTO

---
+++ src/main/java/com/example/crawlertest/domein/VacatureDTO.java
@@ -0,0 +1,32 @@
+package com.example.crawlertest.domein;
+
+public class VacatureDTO {
+
+    private Long id;
+    private String titel;
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
